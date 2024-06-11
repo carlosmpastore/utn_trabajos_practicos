a0b964a726d73ca4a4276b08746912ba2e9a32c1
carlosmpastore <carlosmpastore@gmail.com>
2024-06-11 18:46:09 -0300

    modify app.js file -add forEach function-

--- trabajo_practico_2/app/app.js
+++ trabajo_practico_2/app/app.js
@@ -7,4 +7,15 @@ fetch("https://hp-api.onrender.com/api/characters")
   .then ((data) => {
     const characters = data;
     console.log(characters);
+
+    characters.forEach((character) => {
+    
+      //Modificar dato del estado de los personajes
+      let estado
+      if (character.alive == true) {
+        estado = "Vivo";
+      } else {
+        estado = "Muerto";
+      }
+    });
   })
