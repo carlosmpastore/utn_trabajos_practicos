665daa07eb691a9149778fd471a14761bbcb102b
carlosmpastore <carlosmpastore@gmail.com>
2024-06-13 11:05:29 -0300

    change file name app.js to personajes.js modify personajes.js - added iteration to fetch objects of API- modify personajes.html - modified script tag source to link with js file-

---
+++ trabajo_practico_2/app/personajes.js
@@ -0,0 +1,37 @@
+const $grid = document.querySelector(".grid");
+
+fetch("https://hp-api.onrender.com/api/characters")
+  .then((answer) => {
+    return answer.json();
+})
+.then ((data) => {
+  const characters = data;
+  console.log(characters);
+
+  characters.forEach((character) => {
+    
+    //Modificar dato del estado de los personajes
+    let status;
+    if (character.alive == true) {
+      status = "Vivo";
+    } else {
+      status = "Muerto";
+    }
+
+    //Asignar una imagen alojada en repositorio local para los objetos que no tengan una
+    let imageSrc = character.image;
+    if (!imageSrc) {
+      imageSrc = "../assets/no_image_character.webp"
+    }
+
+    //Agregar al div de clase .grid un div con clase .card para cada objeto del API
+    $grid.innerHTML += `<div class="card">
+    <img src="${imageSrc}" alt="foto de ${character.name}">
+    <p>Género: ${character.gender}</p>
+    <p>Especie: ${character.species}</p>
+    <p>Casa: ${character.house}</p>
+    <p>Estado: ${status}</p>
+    <h4>${character.name}</h4>
+    </div>`;
+  });
+});
