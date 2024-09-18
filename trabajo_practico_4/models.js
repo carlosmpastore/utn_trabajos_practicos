0be1b1fd8848c798654a35b66dee117cf773444d
Carlos M. Pastore <carlosmpastore@gmail.com>
2024-09-18 15:32:07 -0300

    modify: users.json - updated user through function modify: models.js add updateUser function

--- trabajo_practico_4/models.js
+++ trabajo_practico_4/models.js
@@ -73,7 +73,7 @@ const addUser = (userData) => {
     const {name, surname, email, password, isLoggedIn} = userData;
 
     if (!name || !surname || !email || !password) {
-      throw new Error("Missing data: make sure to instert the following information about the user: name, surname, email, password and if it is logged in");
+      throw new Error("Missing data: make sure to instert the following information about the user: name, surname, email, password and if it is logged in - in case it doesn't, you don't have to write anything");
     };
 
     if (typeof name !== "string" || typeof surname !== "string" || typeof email !== "string") {
@@ -116,22 +116,81 @@ const addUser = (userData) => {
   };
 };
 
-const respuesta = addUser({
-  name: "Josemir",
-  surname: "Lujambio",
-  email: "info@lujambio.com",
-  password: "abcd",
-  isLoggedIn: "logged in"
-});
-console.log(respuesta);
+//const respuesta = addUser({
+  //name: "Josemir",
+  //surname: "Lujambio",
+  //email: "info@lujambio.com",
+  //password: "abcd",
+  //isLoggedIn: "logged in"
+//});
+//console.log(respuesta);
 
 // todos los datos del usuario seleccionado se podrían modificar menos el ID
 // si se modifica la pass debería ser nuevamente hasheada
 // si se modifica el email, validar que este no exista
 const updateUser = (userData) => {
   try {
-  } catch (error) {}
+    const {id, name, surname, email, password, isLoggedIn} = userData;
+
+    if (!id) {
+      throw new Error("Insert an ID");
+    };
+
+    const users = getUsers(PATH_FILE_USER);
+
+    const foundUser = users.find((user) => user.id === id);
+
+    if (!foundUser) {
+      throw new Error ("User not found");
+    };
+
+    if (!name || !surname || !email || !password) {
+      throw new Error("Missing data: make sure to instert the following information about the user: name, surname, email, password and if it is logged in - in case it doesn't, you don't have to write anything");
+    };
+
+    if (typeof name !== "string" || typeof surname !== "string" || typeof email !== "string") {
+      throw new Error("Make sure the user's data is a string");
+    };
+
+    if (!email.includes("@")) {
+      throw new Error("The email must include an '@'");
+    };
+
+    const filteredUsers = users.filter((user) => user.id !== id);
+
+    const foundEmail = filteredUsers.find((user) => user.email === email);
+
+    if (foundEmail) {
+      throw new Error("The email already exists");
+    };
+
+    
+    if (name) foundUser.name = name;
+    if (surname) foundUser.surname = surname;
+    if (email) foundUser.email = email;
+    const hash = createHash("sha256").update(password).digest("hex");
+    if (password) foundUser.password = hash;
+    if (isLoggedIn) foundUser.isLoggedIn === "logged in" ? true : false;
+
+    writeFileSync(PATH_FILE_USER, JSON.stringify(users));
+
+    return foundUser;
+
+  } catch (error) {
+    const objError = handleError(error, PATH_FILE_ERROR);
+    return objError;
+  }
 };
+
+const respuesta = updateUser({
+  id:"3d8556a5-7bcf-4910-9403-e93895848cfc",
+  name: "Yosenit",
+  surname: "Lugambio",
+  email: "info@lugambio.com",
+  password: "abcd",
+  isLoggedIn: "",
+});
+console.log(respuesta);
 
 const deleteUser = (id) => {
   try {
